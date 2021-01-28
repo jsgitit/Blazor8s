83202f93cf3edb7d66ccf301873de3b815a5eba1
Jon <jsmartware@aol.com>
2021-01-27 23:25:58 -0800

    removed usings from CardSuit class and removed unnecessary WeatherForecast class.

--- Shared/CardSuit.cs
+++ Shared/CardSuit.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Blazor8s.Shared
+﻿namespace Blazor8s.Shared
 {
     public enum CardSuit
     {
