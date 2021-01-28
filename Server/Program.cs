b55cc74bfd6d1734fa73d6d52804474c31392c66
Jon <jsmartware@aol.com>
2021-01-28 09:16:29 -0800

    Refactored Main() to simplify according to David Pine.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,26 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace Blazor8s.Server
 {
-    public class Program
+    /// <summary>
+    /// 
+    /// Main() was refactored to remove extraneous stuff,
+    /// under the opinion of David Pine.
+    ///
+    /// - removed Public,
+    /// - added Task vs. void, followed by RunAsync() because async is available.
+    /// - eliminated the separate CreateHostBuilder() by moving it into Main()
+    /// 
+    /// Other refacctoring opinions (like removing the entire Main(), and leavin only
+    /// one statement) were ignored due to complexity in readability. 
+    /// 
+    /// </summary>
+    class Program
     {
-        public static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .RunAsync();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
