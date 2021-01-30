87b35b91360e9c6774f3ed8357dae8c43f9b5a8f
Jon <jsmartware@aol.com>
2021-01-30 11:23:00 -0800

    Refactored Card class to record type

--- Shared/Card.cs
+++ Shared/Card.cs
@@ -3,18 +3,30 @@ using System.Threading.Tasks;
 
 namespace Blazor8s.Shared
 {
-    // public record Card2(CardValue value, CardSuit suit);
-    public class Card
+    // This is the old Card class...
+    
+    //public class Card
+    //{
+    //    public Card(CardValue value, CardSuit suit)
+    //    {
+    //        Value = value;
+    //        Suit = suit;
+    //    }
+    //    public CardSuit Suit { get; set; }
+    //    public CardValue Value { get; set; }
+    //    public bool IsFaceDown { get; set; } = true;
+    //    public override string ToString() => $"{Value} of {Suit}"; 
+    //}
+ 
+    
+    // We refactored Card to a record type (value type)
+    public record Card
+    (
+        CardValue Value,  // remember that the order of fields are important, for existing method signatures.
+        CardSuit Suit,
+        bool IsFaceDown = true
+    )
     {
-        public Card(CardValue value, CardSuit suit)
-        {
-            Value = value;
-            Suit = suit;
-        }
-        public CardSuit Suit { get; set; }
-        public CardValue Value { get; set; }
-        public bool IsFaceDown { get; set; } = true;
-        public override string ToString() => $"{Value} of {Suit}"; // expression bodied member
-        
+        public override string ToString() => $"{Value} of {Suit}";
     }
 }
